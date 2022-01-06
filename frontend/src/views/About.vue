fa06f79557bc5284e3439aefbb2b80445f3fb5d7
miyamoto-kentaro <kentaro.miyamoto1001@gmail.com>
2022-01-06 16:41:43 +0900

    sample

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="About">
+    <HelloWorld :user="user" />
   </div>
 </template>
+
+<script lang="ts">
+import {
+  computed,
+  defineComponent,
+  reactive,
+  watchEffect,
+  onMounted,
+} from "vue";
+
+import HelloWorld, { User } from "../components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "About",
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const state = reactive<User>({
+      firstName: "john",
+      lastName: "doe",
+      age: 30,
+    });
+    return {};
+  },
+  data() {
+    return {
+      person: {
+        firstName: "John",
+        lastName: "doe",
+        age: 30,
+      } as User,
+    };
+  },
+  computed: {
+    user(): User {
+      return this.person;
+    },
+  },
+});
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ book.title }}/{{ book.author }}/{{ book.year }}</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+interface Book {
+  title: string;
+  author: string;
+  year: number;
+}
+
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: "Home",
+  data() {
+    return {
+      msg: "Hello TypeScript",
+      book: {
+        title: "Vue 3 Guide",
+        author: "Vue Team",
+        year: 2020,
+      } as Book,
+    };
+  },
+  computed: {
+    getLength(): number {
+      return this.msg.length;
+    },
+  },
+  methods: {
+    changeMsg(msg: string): string {
+      return (this.msg = msg);
+    },
   },
 });
 </script>
