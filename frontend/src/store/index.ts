898069ba905d026fa88b0ae3ea99fe48f0d6f2eb
miyamoto-kentaro <kentaro.miyamoto1001@gmail.com>
2022-01-13 17:28:56 +0900

    commit

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,12 +1,39 @@
-import { createStore } from 'vuex'
+import { InjectionKey } from "vue";
+import { createStore, Store, useStore as baseUseStore } from "vuex";
 
-export default createStore({
-  state: {
+export interface GlobalState {
+  count: number;
+}
+
+export const StateKey: InjectionKey<Store<GlobalState>> = Symbol();
+
+export const store = createStore({
+  state() {
+    return {
+      count: 0,
+    };
   },
+
   mutations: {
+    // increment(state: GlobalState) {
+    //   state.count++;
+    //   localStorage.setItem("count", String(state.count));
+    // },
+    // initializeStore(state: GlobalState) {
+    //   console.log("yes");
+    //   if (localStorage.getItem("count")) {
+    //     state.count = Number(localStorage.getItem("count"));
+    //   } else {
+    //     state.count = 0;
+    //   }
+    // },
+    // setCount(state: GlobalState, count: number) {
+    //   state.count = count;
+    // },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+});
+
+// useState を呼び出す度、 StateKey で型を定義するのを避けるために、ここであらかじめ定義する
+export function useStore() {
+  return baseUseStore(StateKey);
+}
