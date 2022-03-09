83a85b86359fc2dc693a7a647376bc575021b717
skillet-sys <kaito.goforward0912@gmail.com>
2022-03-09 11:17:51 +0900

    20220309

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,34 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
-import CreateReport from "../views/CreateReport.vue";
-import MdEditor from "../views/MdEditor.vue";
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/create-report",
-    name: "CreateReport",
-    component: CreateReport,
-  },
-  {
-    path: "/md-editor",
-    name: "MdEditor",
-    component: MdEditor,
-  },
-];
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
-
-export default router;
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
 import SignInWithEmail from "../views/user_auth/SignInWithEmail.vue";
 import WaitingEmail from "../views/user_auth/WaitingEmail.vue";
 import EmailAuthentication from "../views/user_auth/EmailAuthentication.vue";
@@ -41,11 +12,13 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "Home",
     component: Home
-  }, {
-    path: "/sign-in/email/send-mail",
-    name: "SignInWithEmail",
-    component: SignInWithEmail
-  }, {
+  },
+  // {
+  //   path: "/sign-in/email/send-mail",
+  //   name: "SignInWithEmail",
+  //   component: SignInWithEmail
+  // },
+  {
     path: "/sign-in/email/waiting-email",
     name: "WaitingEmail",
     component: WaitingEmail
@@ -65,15 +38,6 @@ const routes: Array<RouteRecordRaw> = [
     path: "/log-in",
     name: "LogIn",
     component: LogIn
-  }, {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(
-      /* webpackChunkName: "about" */
-      "../views/About.vue")
   }
 ];
 
